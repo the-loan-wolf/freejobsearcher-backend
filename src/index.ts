2390560f5faf63f6a5b13f3a625269a283955aee
rishav <rishavb518@gmail.com>
2025-10-05 00:22:23 +0530

    installed scalar package to render our openapi reference beautifully

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { OpenAPIHono } from '@hono/zod-openapi'
+import { OpenAPIHono } from "@hono/zod-openapi";
 import packageJSON from "../package.json";
-import { swaggerUI } from '@hono/swagger-ui';
-import auth from './routes/auth';
-import { db } from './routes/db';
+import { swaggerUI } from "@hono/swagger-ui";
+import auth from "./routes/auth";
+import { db } from "./routes/db";
+import { Scalar } from "@scalar/hono-api-reference";
 
 const app = new OpenAPIHono({ strict: false });
 
@@ -18,9 +19,18 @@ app.get(
   "/reference",
   swaggerUI({
     url: "/doc",
-  }),
+  })
 );
 
-app.route('/', auth)
-app.route('/', db)
-export default app
+app.get(
+  "/",
+  Scalar((c) => {
+    return {
+      url: "/doc",
+    };
+  })
+);
+
+app.route("/", auth);
+app.route("/", db);
+export default app;
